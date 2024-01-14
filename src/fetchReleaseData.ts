c1996f649fc9358d6596ba4a368e2269697d1fec
Drew Mendenhall <drew@mendenhall.io>
2024-01-14 07:13:01 -0700

    Remove Unneeded Typing

--- src/fetchReleaseData.ts
+++ src/fetchReleaseData.ts
@@ -1,12 +1,12 @@
 import data from './data'
 
-function delay(ts: number): Promise<void> {
+function delay(ts: number) {
   return new Promise(function (resolve) {
     setTimeout(resolve, ts)
   })
 }
 
-export async function fetchReleaseData(): Promise<typeof data> {
+export async function fetchReleaseData() {
   await delay(300 + Math.random() * 500)
   return data
 }
